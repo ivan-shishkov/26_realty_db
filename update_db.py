78454801be3d4a9005f9a2c693101a463ce7dfcf
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-11-12 16:07:39 +0300

    Added getting of path of source JSON file from command line arguments

--- update_db.py
+++ update_db.py
@@ -1,5 +1,6 @@
 import os.path
 import json
+import argparse
 
 
 def load_json_data(filepath):
@@ -9,8 +10,25 @@ def load_json_data(filepath):
         return json.load(file)
 
 
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--filepath',
+        help='a JSON file with ads info for database update '
+             '(default value: ads.json)',
+        default='ads.json',
+        type=str,
+    )
+    command_line_arguments = parser.parse_args()
+
+    return command_line_arguments
+
+
 def main():
-    pass
+    command_line_arguments = parse_command_line_arguments()
+
+    filepath = command_line_arguments.filepath
 
 
 if __name__ == '__main__':

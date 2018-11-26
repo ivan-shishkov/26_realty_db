882a24e12019ce334fca0a7623ea012cf7b2a15b
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-11-26 20:19:33 +0300

    Added pagination for display of ads

--- server.py
+++ server.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_paginate import Pagination, get_page_parameter
+from sqlalchemy_pagination import paginate
+
+from db import db_session, Ad
 
 app = Flask(__name__)
 
+ADS_PER_PAGE = 15
+
+
 @app.route('/')
 def ads_list():
-    return render_template('ads_list.html', ads=[{
-            "settlement": "Череповец",
-            "under_construction": False,
-            "description": '''Квартира в отличном состоянии. Заезжай и живи!''',
-            "price": 2080000,
-            "oblast_district": "Череповецкий район",
-            "living_area": 17.3,
-            "has_balcony": True,
-            "address": "Юбилейная",
-            "construction_year": 2001,
-            "rooms_number": 2,
-            "premise_area": 43.0,
-        }]*10
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+
+    ads_page = paginate(
+        query=db_session.query(Ad).filter(Ad.active),
+        page=page,
+        page_size=ADS_PER_PAGE,
     )
+    pagination = Pagination(
+        page=page,
+        per_page=ADS_PER_PAGE,
+        total=ads_page.total,
+        bs_version=3,
+    )
+    return render_template(
+        'ads_list.html',
+        ads=ads_page.items,
+        pagination=pagination,
+    )
+
 
 if __name__ == "__main__":
     app.run()
